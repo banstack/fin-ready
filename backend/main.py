e4bac2cd203178d22f8bc8dc7bbc4ea5efda6f66
Bryan Montalvan <bmontalvan100@gmail.com>
2026-02-01 12:24:44 -0500

    fix: change query string

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 from . import service
+
 """
 Entry point for FastAPI
 """
-
 app = FastAPI(
     title="FinReady API",
     description="API that connects you to yfinance data",
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-@app.get("/")
+@app.get("/api/v1")
 def read_root():
     return {
         "message": "Welcome to the FinReady API",
@@ -29,21 +29,21 @@ def read_root():
         "version": "1.0.0"
     }
 
-
 """
     GET historical data 
-    Example string: "/historical/NVDA?period=1d
+    Example query string: /historical/NVDA?period=1d
 """
-@app.get("/historical/{security_ticker}")
+@app.get("/api/v1/historical/{security_ticker}")
 async def fetch_historical_security_data(security_ticker: str, period: str):
     if period:
-        security_data = service.fetch_single_security(security_ticker, period)
+        security_data = service.fetch_historical_security_data(security_ticker, period)
     return security_data
 
 """
     Get data pertaining to security (includes basic financials)
+    Example query string: /info/NVDA"
 """
-@app.get("/info/{security_ticker}")
+@app.get("/api/v1/info/{security_ticker}")
 async def read_full_security_info(security_ticker: str):
     if security_ticker:
         security_data = service.fetch_full_security_metadata(security_ticker)
